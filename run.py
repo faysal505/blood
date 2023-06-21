e5a684e3788969a6f05f63923b2faa6909e9d774
faysal505 <127836350+faysal505@users.noreply.github.com>
2023-06-21 17:04:33 +0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def form():
+    return render_template('test.html')
+
+@app.route('/report', methods=['GET', 'POST'])
+def report():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        blood = request.form['blood']
+        report = request.form['report']
+        return render_template('blood.html')
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run()
